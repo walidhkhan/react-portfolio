40308481d5ba32f940571c83f1370a2c05bc1fbe
walidhkhan <walidhkhan@gmail.com>
2021-10-31 21:18:34 -0700

    testing deployment

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,8 @@ function AboutMe() {
     
     return( 
         <div>
-            <h2>About Me</h2>
+            <h2>A little bit about myself...</h2>
+            <p> I am an aspiring web developer who started my coding journey in June 2021 with University of Arizona's online full-stack web development bootcamp program. I love learning about and working with new technologies. </p>
         </div>
     )
 }
